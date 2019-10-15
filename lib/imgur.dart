c09e8929e835740ac109f7b46f47bff7f3f171df
nicolasgarde <nicolas.garde@epitech.eu>
2019-10-15 23:41:59 +0200

    End authentificateClient

--- lib/imgur.dart
+++ lib/imgur.dart
@@ -8,7 +8,6 @@ import 'package:oauth2/oauth2.dart' as oauth2;
 //id ca42024bf4b47ff
 //secret 3688f84bd14578f16f3848bdd8fef68385df0a3e
 // refresh token d107b4fa46bf7a727872ba5f3b9f5a76e26d82a3
-
 class Imgur {
   String _clientId;
   String _clientSecret;
@@ -16,9 +15,8 @@ class Imgur {
   Imgur(this._clientId, this._clientSecret);
 
   authentificateClient(String refreshToken) async {
-    Map<String, String> headers = {"Content-type": "application/json"};
-    String body = '{refresh_token: $refreshToken, client_id: $_clientId, client_secret: $_clientSecret, grant_type: refresh_token}';
-    http.Response request = await http.post(Uri.parse("https://api.imgur.com/oauth2/token"), body: body, headers: headers);
+    var body = {"refresh_token": refreshToken, "client_id": _clientId, "client_secret": _clientSecret, "grant_type": "refresh_token"};
+    http.Response request = await http.post(Uri.parse("https://api.imgur.com/oauth2/token"), body: body);
     // request.add(utf8.encode(json.encode(body)));
     // HttpClientResponse response = await request.close();
     print("bhbh");
