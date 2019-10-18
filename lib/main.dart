bda372a9a6b2fb6a336a2f4b901b8319be6b9b5b
Nicolas <nicolasgarde@hotmail.fr>
2019-10-19 00:21:22 +0200

    Auto stash before merge of "API" and "origin/dev"

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,13 @@ import 'package:epicture/login.dart';
 import 'dart:convert';
 import 'dart:async';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:epicture/imgur.dart';
+import 'package:oauth2/oauth2.dart' as oauth2;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
+  // Imgur imgur = new Imgur("ca42024bf4b47ff", "3688f84bd14578f16f3848bdd8fef68385df0a3e");
+  // await imgur.authentificateClient('d107b4fa46bf7a727872ba5f3b9f5a76e26d82a3');
+  // print(await imgur.accountBase().then((ImgurResponse res) {
+    // return res.data;
+  // }));
