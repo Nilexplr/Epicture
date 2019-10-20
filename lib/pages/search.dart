a497cf820b51146e5c49ca2c1a44b8214b4d5b4d
Andy Begin <Osmonoz@localhost.localdomain>
2019-10-20 17:38:03 +0200

    :heavy_check_mark: feat(search): ending the search part

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:epicture/API/imgur.dart';
 import 'package:epicture/pages/popupImg.dart';
+import 'package:epicture/pages/profile.dart';
+
 
 class Search extends StatefulWidget {
   final Imgur wrapper;
@@ -14,8 +16,24 @@ class Search extends StatefulWidget {
 
 class _Search extends State<Search> {
   final Imgur wrapper;
+  List<Map<String, dynamic>> _listResult = new List<Map<String, dynamic>>();
+  List<Widget> _listWidgetDisp = new List<Widget>();
 
   _Search({Key key, @required this.wrapper});
+
+  void clear() {
+    _listResult.clear();
+  }
+
+  void addImg(Map<String, dynamic> img) {
+    _listResult.add(img);
+  }
+
+  void rebuildStatsList() {
+    setState(() {
+      _listWidgetDisp = buildList(_listResult, context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +45,7 @@ class _Search extends State<Search> {
             top: 50,
             left: 20,
             width: 350,
-            child: SearchBar(),
+            child: SearchBar(wrapper: wrapper, daddy: this),
           ),
           Positioned(
             top: 130,
@@ -35,18 +53,16 @@ class _Search extends State<Search> {
             width: 382,
             height: 545,
             child: Container(
-              color: Colors.red,
-              child: /*GridView.extent(
+              color: Colors.transparent,
+              child: GridView.extent(
+                shrinkWrap : true,
                 maxCrossAxisExtent: 150,
                 padding: const EdgeInsets.all(4),
                 mainAxisSpacing: 4,
                 crossAxisSpacing: 4,
                 childAspectRatio: 0.85,
-                children: <Widget>[
-                  Text('here your result'),
-                  //here the search result
-                ],
-              ),*/Row(children: <Widget>[Text('here will be the result place')],)
+                children: _listWidgetDisp,
+              ),
             )
           ),
         ],
@@ -57,8 +73,28 @@ class _Search extends State<Search> {
 
 class SearchBar extends StatelessWidget {
 
+  final Imgur wrapper;
+  final _Search daddy;
+
+  SearchBar({Key key, @required this.wrapper, @required this.daddy}): super(key: key);
+
   handleKeyPress(String search) {
     print(search);
+    wrapper.searchGallery(search).then((ImgurResponse res) {
+      if (res.data != null) {
+        daddy.clear();
+        res.data.forEach((dynamic gallery) {
+          if (gallery['images'] != null) {
+            gallery['images'].forEach((dynamic img) {
+              daddy.addImg(img);
+            });
+          } else if (gallery != null) {
+              daddy.addImg(gallery);
+          }
+        });
+        daddy.rebuildStatsList();
+      }
+    });
   }
 
   @override
@@ -73,7 +109,7 @@ class SearchBar extends StatelessWidget {
         padding: EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15),
         child: TextField(
           onChanged: handleKeyPress,
-          cursorColor: Colors.green,
+          cursorColor: Colors.white,
           style: TextStyle(
             color: Colors.blue,
             fontSize: 20,
